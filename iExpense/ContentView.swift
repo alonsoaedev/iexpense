1403b8e58397ff6fe6d3e2bcd6a30d1244b6927f
Alonso Acosta <alonso.ae@hotmail.com>
2025-10-18 10:39:05 -0600

    Showing and hiding views

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -5,25 +5,32 @@
 //  Created by Alonso Acosta Enriquez on 17/10/25.
 //
 
-import Observation
 import SwiftUI
 
-@Observable
-class User {
-    var firstName: String = "Bilbo"
-    var lastName: String = "Baggins"
+struct SecondView: View {
+    @Environment(\.dismiss) var dismiss
+    let name: String
+    
+    var body: some View {
+        Text("Hello, \(name)!")
+            .font(.title)
+        
+        Button("Dismiss") {
+            dismiss()
+        }
+    }
 }
 
 struct ContentView: View {
-    @State private var user: User = User()
+    @State private var showSheet: Bool = false
     
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName)")
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
+        Button("Show Sheet") {
+            showSheet.toggle()
         }
-        .padding()
+        .sheet(isPresented: $showSheet) {
+            SecondView(name: "@alonsoae.dev")
+        }
     }
 }
 
