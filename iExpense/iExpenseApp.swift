2eb0a109f1327f52edffaa7b9b4ed524d8b62b3c
Alonso Acosta <alonso.ae@hotmail.com>
2025-10-17 19:52:57 -0600

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Alonso Acosta Enriquez on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
