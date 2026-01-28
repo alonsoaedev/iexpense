43e04facb6f19a49665573c2c7f4b1924f66637f
Alonso Acosta <alonso.ae@hotmail.com>
2026-01-28 15:07:08 -0600

    Edit the name of the expense from the navigation title instead of a textfield

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct AddView: View {
     @Environment(\.dismiss) var dismiss
     
-    @State private var name = ""
+    @State private var name = "Name"
     @State private var type = "Personal"
     @State private var currency: String = "USD"
     @State private var amount = 0.0
@@ -23,8 +23,6 @@ struct AddView: View {
     var body: some View {
         NavigationStack {
             Form {
-                TextField("Name", text: $name)
-                
                 Picker("Type", selection: $type) {
                     ForEach(types, id: \.self) {
                         Text($0)
@@ -39,7 +37,8 @@ struct AddView: View {
                 TextField("Amount", value: $amount, format: .currency(code: currency))
                     .keyboardType(.decimalPad)
             }
-            .navigationTitle("Add new expense")
+            .navigationTitle($name)
+            .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 Button("Save") {
                     expenses.items.append(
